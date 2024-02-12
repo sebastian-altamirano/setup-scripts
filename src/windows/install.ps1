d5cbee6ffb229f64728288111e3e44902840b945
Sebastián Altamirano <38230545+sebastian-altamirano@users.noreply.github.com>
2024-02-12 19:40:14 -0300

    Add JSON Schema validation in Windows

--- src/windows/install.ps1
+++ src/windows/install.ps1
@@ -4,4 +4,8 @@ winget install `
 	--exact --id --silent --accept-package-agreements --accept-source-agreements `
 	Microsoft.PowerShell
 
+if (!(Test-Json -Path "../../config/settings.json" -SchemaFile "../../config/settings.schema.json")) {
+	throw 'Failed to validate `settings.json` with `settings.schema.json`, fix the errors and try again.'
+}
+
 pwsh .\install-part-1.ps1
