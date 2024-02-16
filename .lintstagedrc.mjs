739499a3f34623e32d259354e592091a30cc9b15
Sebastián Altamirano <38230545+sebastian-altamirano@users.noreply.github.com>
2024-02-15 21:24:35 -0300

    Fix lint-staged configuration for Python files

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,22 +1,50 @@
 /// <reference types="lint-staged" />
 
-/** @param {string} fileName */
+import { existsSync } from "node:fs";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+/**
+ * @param {string} fileName
+ *
+ * @return {string}
+ */
 function formatPowerShellScript(fileName) {
 	return `scripts/format-powershell.ps1 \
 -Path ${fileName} \
 -SettingsPath src/windows/formatter-settings.psd1`;
 }
 
-/** @param {string} fileName */
+/**
+ * @param {string} fileName
+ *
+ * @return {string}
+ */
 function lintPowerShellScript(fileName) {
 	return `scripts/lint-powershell.ps1 \
 -Path ${fileName} \
 -SettingsPath src/windows/linter-settings.psd1`;
 }
 
-/** @param {string} fileName */
+/**
+ * @param {string} fileName
+ *
+ * @example
+ * getPythonTestFileName("foo_test.py") // "foo_test.py"
+ * @example
+ * getPythonTestFileName("foo.py") // "foo_test.py" (if it exists)
+ * @example
+ * getPythonTestFileName("foo.py") // null (if `foo_test.py` doesn't exist)
+ *
+ * @returns {string | null}
+ */
 function getPythonTestFileName(fileName) {
-	return fileName.endsWith("_test.py") ? fileName : `${fileName.slice(0, -3)}_test.py`;
+	if (fileName.endsWith("_test.py")) {
+		return fileName;
+	}
+
+	const testFileName = `${fileName.slice(0, -3)}_test.py`;
+	return existsSync(testFileName) ? testFileName : null;
 }
 
 /**
@@ -35,8 +63,8 @@ export default {
 		/** @type {Set<string>} */
 		const testFileNames = new Set([]);
 
-		return fileNames.flatMap((fileName) => {
-			const operations = [
+		const operations = fileNames.flatMap((fileName) => {
+			const fileOperations = [
 				`pdm run -p src/ubuntu isort ${fileName}`,
 				`pdm run -p src/ubuntu black ${fileName}`,
 				`pdm run -p src/ubuntu mypy ${fileName}`,
@@ -45,13 +73,21 @@ export default {
 
 			const testFileName = getPythonTestFileName(fileName);
 			// Run the test only if it has not yet run.
-			if (!testFileNames.has(testFileName)) {
+			if (testFileName && !testFileNames.has(testFileName)) {
 				testFileNames.add(testFileName);
-				operations.push(`pdm run -p src/ubuntu unittest ${testFileName}`);
+				fileOperations.push(`pdm run -p src/ubuntu unittest ${testFileName}`);
 			}
 
-			return operations;
+			return fileOperations;
 		});
+		// Always run static analysis tests.
+		operations.push(
+			`pdm run -p src/ubuntu unittest ${dirname(
+				fileURLToPath(import.meta.url),
+			)}/src/ubuntu/dotfiles/tests/static_analysis_test.py`,
+		);
+
+		return operations;
 	},
 	"config/settings?(.schema).json": "scripts/validate-settings.ps1",
 	"src/ubuntu/pyproject.toml": "npm run update-linux-requirements",
